944d8958919d06c99763238eb55c35065186642a
mohan43u <mohan43u@gmail.com>
2014-04-02 12:59:11 +0530

    revamped and fine tuned

--- example/googleresttest.js
+++ example/googleresttest.js
@@ -1,11 +1,7 @@
 var fs = require('fs');
-var os = require('os');
 var url = require('url');
 var http = require('http');
-var path = require('path');
 var GoogleRest = require('../lib/googlerest');
-var querystring = require('querystring');
-var DuplexBufferStream = require('duplexbufferstream');
 
 var dbfilename = 'googlerest.js.db';
 try { var google_tokens = JSON.parse(fs.readFileSync(dbfilename)).google_tokens; } catch(e) {}
@@ -13,72 +9,65 @@ var googlerest = new GoogleRest({'google_tokens': google_tokens});
 
 var server = http.createServer(function(req, res) {
     var pathname = url.parse(req.url).pathname;
-    var apiresult = new DuplexBufferStream();
 
     console.log(pathname);
 
     if(pathname.match("/google/authorize$")) {
-	apiresult.pipe(res);
-	googlerest.authorize(req, apiresult);
+	googlerest.authorize(req, function(response){
+	    this.writeHead(response.statusCode, response.headers);
+	    response.pipe(this);
+	}.bind(res));
     }
     else if(pathname.match("/google/authorized$")) {
-	apiresult.on('finish', function(res, apiresult) {
-	    var apiresultbuf = apiresult.read();
-	    var tokens = JSON.parse(apiresultbuf.toString()).google_tokens;
-	    if(tokens) {
-		var expire = new Date();
-		expire.setSeconds(expire.getSeconds() + (tokens.expires_in - (60 * 5)));
-		tokens.expires_in = expire;
-		if(google_tokens && google_tokens.refresh_token) tokens.refresh_token = google_tokens.refresh_token;
-		fs.writeFile(dbfilename, JSON.stringify({'google_tokens': tokens}));
-		res.end(JSON.stringify({'oauth2': 'authorized'}));
+	delete req.headers['accept-encoding'];
+	googlerest.get_access_token(req, function(res, dbfilename, response) {
+	    if(response.error) {
+		res.end(JSON.stringify(response));
+	    }
+	    else if(response.statusCode != 200) {
+		res.writeHead(response.statusCode, response.headers);
+		response.pipe(res);
 	    }
 	    else {
-		res.end(apiresultbuf.toString());
+		var body = [];
+		response.on('data', function(chunk) {
+		    this.push(chunk);
+		}.bind(body));
+		response.on('end', function(res, body, dbfilename) {
+		    var tokens = JSON.parse(Buffer.concat(body).toString());
+		    if(this.google_tokens) {
+			for(var k in tokens) {
+			    this.google_tokens[k] = tokens[k]
+			}
+		    }
+		    else {
+			this.google_tokens = tokens;
+		    }
+		    fs.writeFile(dbfilename, JSON.stringify({'google_tokens': this.google_tokens}));
+		    res.end(JSON.stringify({'oauth2': 'authorized'}));
+		}.bind(this, res, body, dbfilename));
 	    }
-	}.bind(this, res, apiresult));
-	googlerest.get_access_token(req, apiresult);
+	}.bind(googlerest, res, dbfilename));
     }
     else if(pathname.match("/google")) {
 	if(!googlerest.google_tokens) {
 	    var redirect_url = new url.Url();
 	    redirect_url.pathname = '/google/authorize';
-	    redirect_url.query = {'access_type': 'offline'};
-	    res.writeHead(303, {
-		'Location': url.format(redirect_url)
-	    });
-	    res.end();
-	}
-	else if(Date.parse(googlerest.google_tokens.expires_in) < Date.parse(new Date())) {
-	    var redirect_url = new url.Url();
-	    if(googlerest.google_tokens.refresh_token) {
-		redirect_url.pathname = '/google/authorized';
-		redirect_url.query = {'refresh_token': googlerest.google_tokens.refresh_token};
-	    }
-	    else {
-		redirect_url.query = {'access_type': 'offline'};
-		redirect_url.pathname = '/google/authorize';
-	    }
 	    res.writeHead(303, {
 		'Location': url.format(redirect_url)
 	    });
 	    res.end();
 	}
 	else {
-	    pathname = pathname.substring(pathname.indexOf('/', 1));
-	    apiresult.pipe(res);
-	    if(req.method == 'POST') {
-		var data = new DuplexBufferStream();
-		req.pipe(data);
-		data.on('finish', function(data, req, apiresult, pathname) {
-		    options = querystring.parse(data.read().toString());
-		    googlerest.call_google(req, apiresult, pathname, options);
-		}.bind(this, data, req, apiresult, pathname));
-	    }
-	    else {
-		options = url.parse(req.url, true).query;
-		googlerest.call_google(req, apiresult, pathname, options);
-	    }
+	    var gurl = url.parse(req.url, true);
+	    gurl.pathname = pathname.substring(pathname.indexOf('/', 1));
+	    delete gurl.protocol;
+	    delete gurl.host;
+	    req.url = url.format(gurl);
+	    googlerest.call_google(req, null, function(response){
+		this.writeHead(response.statusCode, response.headers);
+		response.pipe(this);
+	    }.bind(res));
 	}
     }
     else {
